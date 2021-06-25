01f8bb87138b23f6242cc0a3cb11fdbc6f53dc0f
Rune Botten <rbotten@gmail.com>
2021-06-25 11:00:24 -0700

    Set apiVersion on client if supported

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,11 @@
 import _client from 'part:@sanity/base/client';
-const client = _client as import('@sanity/client').SanityClient;
+const sanityClient = _client as import('@sanity/client').SanityClient;
+let client = sanityClient
+
+if (typeof sanityClient.withConfig === 'function') {
+  client = sanityClient.withConfig({
+    apiVersion: "v1"
+  })
+}
+
 export default client;
