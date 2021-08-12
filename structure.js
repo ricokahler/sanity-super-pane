5b8b8b60cca356fc9f97fe785f1e5c64ad7cb1c1
Rico Kahler <ricokahler@me.com>
2021-08-11 21:43:21 -0400

    just configuring the deploy preview

--- structure.js
+++ structure.js
@@ -3,7 +3,7 @@ import { createSuperPane } from './super-pane';
 
 export default () =>
   S.list()
-    .title('Base')
+    .title('TESTING')
     .items([
       S.listItem().title('Normal List').child(createSuperPane('movie', S)),
     ]);
